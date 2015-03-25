6aa1d5d654d93d3b6e9128c513e3f92ca22d0bbf
Gregory14 <gregory.joly.14@gmail.com>
2015-03-25 11:54:33 +0100

    Initialisation site planète

---
+++ enigma/site-web/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Les planètes !</title>
+	<link rel="stylesheet" href="desktop.css">
+	<link rel="stylesheet" href="mobile.css">
+</head>
+<body>
+	
+</body>
+</html>
